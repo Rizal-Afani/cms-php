0049d9b3cc3635c6581195b9e011a25d48179626
Rizal-Afani <ahmadrizalafani@gmail.com>
2013-05-24 14:36:44 +0700

    contoh cms php

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php require "function.php"; cek_login(); ?>
+<html>
+<head>
+<title>Admin MOS Template</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Copyright" content="arirusmanto.com">
+<meta name="description" content="Admin MOS Template">
+<meta name="keywords" content="Admin Page">
+<meta name="author" content="Ari Rusmanto">
+<meta name="language" content="Bahasa Indonesia">
+
+<link rel="shortcut icon" href="stylesheet/img/devil-icon.png"> <!--Pemanggilan gambar favicon-->
+<link rel="stylesheet" type="text/css" href="css/style.css"> <!--pemanggilan file css-->
+<script type="text/javascript" src="js/ckeditor/ckeditor.js"></script>
+<script type="text/javascript" src="js/jquery-1.9.1.js"></script>
+</head>
+
+<body>
+<div id="header">
+	<div class="inHeader">
+		<div class="mosAdmin">
+        <?php $user = ""; if(isset($_SESSION['user'])){ ?>
+		Hallo, <?php echo $_SESSION['user']['nama_lengkap']; ?><br>
+        <?php } ?>
+		<a href="">Lihat website</a> | <a href="">Help</a> | <a href="logout.php">Keluar</a>
+		</div>
+	<div class="clear"></div>
+	</div>
+</div>
+
+<div id="wrapper">
+	<div id="leftBar">
+	<ul>
+		<li><a href="index.php?page=content">Content</a></li>
+		<li><a href="index.php?page=user">User</a></li>
+		<li><a href="index.php?page=categorie">Categorie</a></li>
+        <li><a href="index.php?page=option">Option</a></li>
+	</ul>
+	</div>
+	<?php 	
+		if(!isset($_GET["page"])){
+			require "content.php"; 
+		}else{
+			if($_GET['page'] == "content"){
+				require "content.php";
+			}else if($_GET['page'] == "user"){
+				require "user.php";
+			}else if($_GET['page'] == "categorie"){
+				require "kategori.php";
+			}else if($_GET['page'] == "write"){
+				require "write.php";
+			}else if($_GET['page'] == "option"){
+				require "password.php";
+			}else{
+				echo "<div id='rightContent'><h2 style='color:red;'>Page not found !!</h2></div>";
+			}
+		}
+	?>
+<div class="clear"></div>
+<div id="footer">
+	&copy; 2012 MOS css template | <a href="javascript:void(0)">free css template</a> | programming by <a href="http://calonpresident.blogspot.com" target="_blank">calonpresident.blogspot.com</a><br>
+	redesign and programming <a href="http://facebook.com/ahmad.rizal.afani" target="_blank">Ahmad Rizal Afani</a>
+</div>
+</div>
+</body>
+</html>
